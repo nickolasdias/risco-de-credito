3518c9b307188c21e217ed5861111a490c3410cd
Nickolas Leitão Dias <64763746+nickolasdias@users.noreply.github.com>
2021-12-13 15:12:38 -0300

    Initial commit

---
+++ api/handler.py
@@ -0,0 +1,45 @@
+import pickle as pi
+import pandas as pd
+from flask import Flask, request, Response
+from riscodecredito.Riscodecredito import riscodecredito
+
+# Carregando Modelo
+modelo = pi.load(open('/home/nickolas/Área de Trabalho/Portfólio/pa005/modelo/random_forest_best.pkl', 'rb'))
+
+# Inicializando API
+app = Flask(__name__)
+@app.route('/riscodecredito/predicao', methods=['POST'])
+
+def risco_de_credito_predicao():
+    teste_json = request.get_json()
+    
+    if teste_json: 
+        if isinstance(teste_json, dict): # Existe um dado
+            teste_raw = pd.DataFrame(teste_json, index=[0])
+        
+        else: # Multiplos dados
+            teste_raw = pd.DataFrame(teste_json, columns=teste_json[0].keys())
+            
+        # Instanciar classe credito
+        pipeline = riscodecredito()
+        
+        # descrição dos dados
+        df1 = pipeline.descricao_dados(teste_raw)
+        
+        # preprocessamento_1
+        df2 = pipeline.pre_processamento_1(df1)
+        
+        # preprocessamento_2
+        df3 = pipeline.pre_processamento_2(df2)
+        
+        # Predições
+        df_response = pipeline.get_prediction(modelo, teste_raw, df3)
+        
+        return df_response
+         
+    else:
+        return Response('{}', status=200, mimetype='application/json')
+    
+
+if __name__ == '__main__':
+    app.run('0.0.0.0')
